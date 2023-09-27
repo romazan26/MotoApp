31fa3840967a37af712db3a288ed17290399fa26
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-09-27 21:51:57 +0300

    good login view

---
+++ MotoApp/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MotoApp
+//
+//  Created by Роман on 27.09.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
