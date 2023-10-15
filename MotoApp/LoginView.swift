42bcca7f0b1cff3087f4631e1c552d2f1f96f46b
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-10-15 15:55:54 +0300

    FinalLoginView

--- MotoApp/LoginView.swift
+++ MotoApp/LoginView.swift
@@ -8,64 +8,65 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var login = ""
+    @State private var userName = ""
     @State private var password = ""
     @State private var isPresented = false
+    
     @FocusState private var nameIsFocused: Bool
+    
+    private var isLoginButtonDisable: Bool {
+        userName.isEmpty || password.isEmpty
+    }
     
     var body: some View {
         
-        VStack (spacing: 20){
-            Image("moto")
-                .resizable()
-                .frame(width: 380, height: 220)
-                .offset(y: -30)
-            Text("Добро пожаловать в Гараж")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .shadow(radius: 5)
-            TextField("login...", text: $login)
-                .costomStyle()
-                .focused($nameIsFocused)
-            SecureField("password...", text: $password)
-                .costomStyle()
-                .focused($nameIsFocused)
-            
-            HStack {
-                Button(action: { isPresented.toggle()},
-                       label: {
-                    Text("Вход")
-                        .foregroundStyle(.white)
-                        .font(.title2)
-                        .bold()
-                })
-                .frame(width: 160, height: 60)
-                .background(LinearGradient(
-                    colors: [.black, .blue.opacity(0.5)],
-                    startPoint: .bottomLeading,
-                    endPoint: .topTrailing))
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .sheet(isPresented: $isPresented, content: {
-                    ContentView()
-                })
+        ZStack {
+            Color.black.opacity(0.15)
+                .ignoresSafeArea()
+            VStack (spacing: 20){
+                Image("moto")
+                    .resizable()
+                    .frame(width: 380, height: 220)
+                    .offset(y: -30)
+                Text("Добро пожаловать в Гараж")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .shadow(radius: 5)
+                TextField("login...", text: $userName)
+                    .costomStyle()
+                    .focused($nameIsFocused)
+                SecureField("password...", text: $password)
+                    .costomStyle()
+                    .focused($nameIsFocused)
                 
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("Новый Гараж")
-                        .foregroundStyle(.white)
-                        .font(.title2)
-                        .bold()
-                })
-                .frame(width: 160, height: 60)
-                .background(LinearGradient(
-                    colors: [.black, .blue.opacity(0.5)],
-                    startPoint: .bottomLeading,
-                    endPoint: .topTrailing))
-                .clipShape(RoundedRectangle(cornerRadius: 15))
+                HStack {
+                    Button(action: { isPresented.toggle()},
+                           label: {
+                        Text("Вход")
+                            .foregroundStyle(.white)
+                            .font(.title2)
+                            .bold()
+                    })
+                    .frame(width: 160, height: 60)
+                    .background(LinearGradient(
+                        colors: isLoginButtonDisable
+                        ?[.gray.opacity(0.6)]
+                        :[.black, .blue.opacity(0.5)],
+                        startPoint: .bottomLeading,
+                        endPoint: .topTrailing))
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .sheet(isPresented: $isPresented, content: {
+                        ContentView()
+                    })
+                    .disabled(isLoginButtonDisable)
+                    
+                    ButtonView(action: {}, label: "Новый Гараж")
+                }
             }
+            .padding()
+            .onTapGesture {
+                nameIsFocused = false
         }
-        .padding()
-        .onTapGesture {
-            nameIsFocused = false
         }
     }
 }
@@ -80,7 +81,7 @@ struct TFStyleViewModifier: ViewModifier {
         content
             .padding()
             .frame(width: 320)
-            .background(.gray.opacity(0.1))
+            .background(.gray.opacity(0.2))
             .clipShape(RoundedRectangle(cornerRadius: 15))
     }
 }
@@ -88,5 +89,24 @@ struct TFStyleViewModifier: ViewModifier {
 extension View {
     func costomStyle() -> some View {
         modifier(TFStyleViewModifier())
+    }
+}
+
+struct ButtonView: View {
+    let action: () -> Void
+    let label: String
+    var body: some View {
+        Button(action: action, label: {
+            Text(label)
+                .foregroundStyle(.white)
+                .font(.title2)
+                .bold()
+        })
+        .frame(width: 160, height: 60)
+        .background(LinearGradient(
+            colors: [.black, .blue.opacity(0.5)],
+            startPoint: .bottomLeading,
+            endPoint: .topTrailing))
+        .clipShape(RoundedRectangle(cornerRadius: 15))
     }
 }
