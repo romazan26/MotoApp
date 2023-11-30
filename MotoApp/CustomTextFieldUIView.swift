49515080209cd9330c55aa7a3d5a86b796642d0b
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-11-30 23:46:35 +0300

    new profil textfield

---
+++ MotoApp/CustomTextFieldUIView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextFieldUIView.swift
+//  MotoApp
+//
+//  Created by Роман on 30.11.2023.
+//
+
+import SwiftUI
+
+struct CustomTextFieldUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextFieldUIView()
+}
