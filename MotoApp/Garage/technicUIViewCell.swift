0a43782fc1c688112e94cadacf57512635c92556
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-11-30 11:01:39 +0300

    add cell technic

---
+++ MotoApp/Garage/technicUIViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  tahnicUIViewCell.swift
+//  MotoApp
+//
+//  Created by Роман on 30.11.2023.
+//
+
+import SwiftUI
+
+struct technicUIViewCell: View {
+    let tehnic = Technic()
+    
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    technicUIViewCell()
+}
