33ebd0e2817a1f1890554cd4a749bc0cfa11c373
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-01-18 23:36:54 +0300

    get AppSorage

---
+++ MotoApp/HellowView.swift
@@ -0,0 +1,18 @@
+//
+//  HellowView.swift
+//  MotoApp
+//
+//  Created by Роман on 18.01.2024.
+//
+
+import SwiftUI
+
+struct HellowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HellowView()
+}
