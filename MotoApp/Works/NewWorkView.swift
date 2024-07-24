ef3109b72b264acd89c36dfac875641ba2e43ead
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-07-24 14:27:10 +0300

    end new dising for cell

--- MotoApp/Works/NewWorkView.swift
+++ MotoApp/Works/NewWorkView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct NewWorkView: View {
     @ObservedObject var vm: WorksViewModel
     @Environment(\.dismiss) var dismiss
+    @FocusState private var keyboardIsFocused: Bool
 
     var body: some View {
         ZStack {
@@ -18,14 +19,23 @@ struct NewWorkView: View {
                 
                 //MARK: - Text field group
             VStack(alignment: .leading, spacing: 20) {
+                //MARK: - Title
                 Text("Введите название")
+                
+                //MARK: - work name
                 TextField("Название работы", text: $vm.simpleNamework, axis: .vertical)
                     .font(.system(size: 30, weight: .heavy, design: .serif))
+                    .focused($keyboardIsFocused)
                 
+                //MARK: - mileage
                 CustomTextFieldUIView(text: $vm.simpleOdometr, placeHolder: "одометр")
-                
-                
+                    .focused($keyboardIsFocused)
+                    .keyboardType(.numberPad)
+            
+                //MARK: - Price
                 CustomTextFieldUIView(text: $vm.simplePrice, placeHolder: "Цена")
+                    .focused($keyboardIsFocused)
+                    .keyboardType(.numberPad)
                 
                 Text("Дата: \(String(Date.now.formatted()))")
             }
@@ -39,6 +49,12 @@ struct NewWorkView: View {
             }.padding()
                 .navigationTitle("Новая работа")
         }
+        .onAppear(perform: {
+            vm.clear()
+        })
+        .onTapGesture {
+            keyboardIsFocused = false
+    }
         .minimumScaleFactor(0.5)
     }
 }
